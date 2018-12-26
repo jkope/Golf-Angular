b79285371365f670cbf51543241a66c9da78547d
Jake K <jake.kopenhefer@gmail.com>
2018-12-26 10:48:13 -0700

    create courses service create page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DialogComponent } from './dialog/dialog.component';
+import { ScoreCardComponent } from './score-card/score-card.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'edit', component: DialogComponent}
+  { path: 'edit', component: DialogComponent },
+  { path: 'game', component: ScoreCardComponent },
+  { path: '', component: ScoreCardComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { DialogComponent } from './dialog/dialog.component';
+import { ScoreCardComponent } from './score-card/score-card.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DialogComponent
+    DialogComponent,
+    ScoreCardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
